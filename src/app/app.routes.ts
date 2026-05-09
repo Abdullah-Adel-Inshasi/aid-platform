9892167d33784ffe735358234e917e50a32dbae8
Abdullah Adel <abdullah@gowell.co>
2026-05-09 11:12:14 +0300

    initial setup, main pages, global styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Donor } from './pages/donor/donor';
+import { Recipient } from './pages/recipient/recipient';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'donor', component: Donor },
+  { path: 'recipient', component: Recipient },
+  { path: '**', redirectTo: '' },
+];
---
+++ src/app/pages/recipient/recipient.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipient',
+  imports: [],
+  templateUrl: './recipient.html',
+  styleUrl: './recipient.css',
+})
+export class Recipient {}
